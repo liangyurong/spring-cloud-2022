05fa973479537dd88d5efa13952cf0eb7d1202b6
liangyurong <794842744@qq.com>
2022-04-03 02:14:46 +0800

    4-3

---
+++ spring-cloud-provider-8001-config-yoyoyo/src/main/java/com/kuang/springcloud/dao/DeptDao.java
@@ -0,0 +1,23 @@
+package com.kuang.springcloud.dao;
+
+import com.kuang.springcloud.pojo.Dept;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface DeptDao {
+
+    // 增加一个部门
+    boolean addDept(Dept dept);
+
+    // 根据id查询部门
+    Dept queryById(Long id);
+
+    // 查询所有部门
+    List<Dept> queryAll();
+
+
+}
